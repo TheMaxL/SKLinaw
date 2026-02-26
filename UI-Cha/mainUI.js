3563c5a77e58c34de07fc402f5857d35c3c2a55e
CHACHA1136 <cmvalmores1@up.edu.ph>
2026-02-26 13:09:15 +0800

    revised UI

---
+++ UI-Cha/mainUI.js
@@ -0,0 +1,6 @@
+document.getElementById('signInBtn').addEventListener('click', function() {
+    alert('Sign In functionality coming soon!');
+});
+document.getElementById('publicViewBtn').addEventListener('click', function() {
+    alert('Public View functionality coming soon!');
+});
