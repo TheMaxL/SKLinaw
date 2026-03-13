893cbb8e76dfcee94b3c2cd1e7acecdcd20e2323
selenamalig <sbmalig@up.edu.ph>
2026-03-13 13:07:36 +0800

    Create project.java

---
+++ src/main/java/com/example/sklinaw/project.java
@@ -0,0 +1,24 @@
+package com.example.sklinaw;
+
+public class Project {
+    private String projectName;
+    private String purpose;
+    private String committeeName;
+    private String barangay;
+    private String councilorName;
+    private double totalCost;
+
+    public String getProjectName() { return projectName; }
+    public String getPurpose() { return purpose; }
+    public String getCommitteeName() { return committeeName; }
+    public String getBarangay() { return barangay; }
+    public String getCouncilorName() { return councilorName; }
+    public double getTotalCost() { return totalCost; }
+
+    public void setProjectName(String projectName) { this.projectName = projectName; }
+    public void setPurpose(String purpose) { this.purpose = purpose; }
+    public void setCommitteeName(String committeeName) { this.committeeName = committeeName; }
+    public void setBarangay(String barangay) { this.barangay = barangay; }
+    public void setCouncilorName(String councilorName) { this.councilorName = councilorName; }
+    public void setTotalCost(double totalCost) { this.totalCost = totalCost; }
+}
