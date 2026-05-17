739624c38cb16017f23bc72cc6df974c93e64a26
Max Lennon <lennon@limpag.com>
2026-05-18 07:52:21 +0800

    Test clean URLs

--- src/main/resources/static/Councilor/Home/home.js
+++ src/main/resources/static/Councilor/Home/home.js
@@ -2,24 +2,24 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // 🔹 NAVBAR BUTTONS
   document.getElementById('nav-signup')?.addEventListener('click', () => {
-    location.href = '../Sign-in/signup.html';
+    location.href = '../Sign-in';
   });
 
   document.getElementById('nav-login')?.addEventListener('click', () => {
-    location.href = '../Log-in/Login.html';
+    location.href = '../Log-in/Login';
   });
 
   // 🔹 ENTRY CARDS
   document.getElementById('card-public')?.addEventListener('click', () => {
-    location.href = '../../Public/Feedback/Feedback.html';
+    location.href = '../../Public/Feedback/Feedback';
   });
 
   document.getElementById('card-login')?.addEventListener('click', () => {
-    location.href = '../Log-in/Login.html';
+    location.href = '../Log-in/Login';
   });
 
   document.getElementById('card-signup')?.addEventListener('click', () => {
-    location.href = '../Sign-in/signup.html';
+    location.href = '../Sign-in/signup';
   });
 
 });
