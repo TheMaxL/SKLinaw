86aa469620669f8c6bc9d5a65f787429f57bfa88
Max Lennon <lennon@limpag.com>
2026-05-19 07:36:23 +0800

    Editing the Postgres db

--- src/main/java/com/example/sklinaw/controller/Committeecontroller.java
+++ src/main/java/com/example/sklinaw/controller/Committeecontroller.java
@@ -1,7 +1,6 @@
 package com.example.sklinaw.controller;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Map;
